5d9d3a512e6c9f8699b5d4b3db521ceb7656f152
Abhishek Tiwari <abhie.lp@gmail.com>
2021-11-02 22:56:04 +0530

    Include JWT and send access token on signin

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,24 +3,29 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { UserRepository } from './users.repository';
 import * as bcrypt from "bcrypt";
+import { JwtService } from '@nestjs/jwt';
+import { JWTPayload } from './jwt-payload.interface';
 
 @Injectable()
 export class AuthService {
     constructor(
         @InjectRepository(UserRepository)
-        private userRepository: UserRepository
+        private userRepository: UserRepository,
+        private jwtService: JwtService
     ) {}
 
     signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.userRepository.createUser(authCredentialsDto)
     }
 
-    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{ accessToken: string }> {
         const { username, password } = authCredentialsDto
         const user = await this.userRepository.findOne({ username })
 
         if (user && (await bcrypt.compare(password, user.password))) {
-            return "success"
+            const payload: JWTPayload = { username }
+            const accessToken = await this.jwtService.sign(payload)
+            return { accessToken }
         } else {
             throw new UnauthorizedException("Please check login credentials.")
         }
