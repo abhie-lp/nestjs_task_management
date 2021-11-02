c690165510afe2a57803345985db77c47b442c91
Abhishek Tiwari <abhie.lp@gmail.com>
2021-11-02 22:11:38 +0530

    Implement user signup with field level validation

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,17 @@
+import { IsString, Matches, MaxLength, MinLength } from "class-validator";
+
+export class AuthCredentialsDto {
+    @IsString()
+    @MinLength(4)
+    @MaxLength(32)
+    username: string;
+
+    @IsString()
+    @MinLength(8)
+    @MaxLength(64)
+    @Matches(
+        /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+        { message: "Password is too weak" }
+    )
+    password: string
+}
